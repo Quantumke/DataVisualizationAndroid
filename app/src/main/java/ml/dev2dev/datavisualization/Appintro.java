7fd9371cea62b6d3bd9b2c7d2a214a0336af5d59
Benson Mburu <nguruben@gmail.com>
2015-11-03 22:26:04 +0300

    added imoports

--- app/src/main/java/ml/dev2dev/datavisualization/Appintro.java
+++ app/src/main/java/ml/dev2dev/datavisualization/Appintro.java
@@ -6,9 +6,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Appintro extends AppCompatActivity {
+    static final int TOTAL_PAGES = 4;
 
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+    LinearLayout circles;
+    Button btnSkip;
+    Button btnDone;
+    ImageButton btnNext;
+    boolean isOpaque = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
